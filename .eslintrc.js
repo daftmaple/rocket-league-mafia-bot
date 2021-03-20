a85f147a077e9346ac14303f8f88fb42d428e543
daftmaple <ubuntumaple@gmail.com>
2021-03-21 06:32:44 +1100

    Improved eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,34 @@
-// eslint-disable-next-line no-undef
 module.exports = {
+  root: true,
   env: {
-    browser: true,
-    es2021: true,
+    node: true,
+    jest: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+  ],
+  plugins: ['@typescript-eslint', 'prettier'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 12,
+    project: './tsconfig.json',
+    ecmaVersion: 6,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint'],
+  ignorePatterns: ['.eslintrc.js'],
   rules: {
+    'func-style': ['error', 'declaration', { allowArrowFunctions: true }],
     'linebreak-style': ['error', 'unix'],
     semi: ['error', 'always'],
-    '@typescript-eslint/explicit-module-boundary-types': 0,
+    '@typescript-eslint/explicit-function-return-type': 'warn',
+    '@typescript-eslint/explicit-module-boundary-types': 'warn',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    'import/no-unused-modules': ['warn', { unusedExports: true }],
+    'import/no-default-export': 'error',
+    'prettier/prettier': 'error',
   },
 };
